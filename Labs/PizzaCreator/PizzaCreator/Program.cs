1117ac904ce5c0e84fca9adf3aee94601bee5f6b
jakobrodrigueztcc <jakob.rodriguezfernandez@my.tccd.edu>
2019-01-30 19:39:44 -0600

    PizzaCreator started

---
+++ Labs/PizzaCreator/PizzaCreator/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Pizza Creator (ITSE 1430)
+ * Jakob Rodriguez
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaCreator
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine("Test");
+        }
+    }
+}
